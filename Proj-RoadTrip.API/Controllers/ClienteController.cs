0824b9c7f02786e219ca6ba60cbcbe0f12b3ca36
desenvolvedor1-carrera <desenvolvedor1@carrera.com.br>
2022-08-27 20:04:57 -0300

    add migration e dtbase

---
+++ Proj-RoadTrip.API/Controllers/ClienteController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Proj_RoadTrip.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proj_RoadTrip.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClienteController : ControllerBase
+    {
+        public ClienteController()
+        {
+        }
+
+        [HttpGet]
+        public Cliente Get()
+        {
+            return new Cliente()
+            {
+                ClienteId = 1,
+                Nome = "Douglas",
+                DtNascimento = "20/03/1991",
+                CPF = "11.625.027-83",
+                EstadoCivil = "Solteiro",
+                Email = "lima.mdouglas@gmail.com",
+                TelefoneEmergencia = "(21)98765-4321",
+                ResponsavelEmergencia = "Mãe",
+                Instagram = "teste"
+            };
+        }
+    }
+}
---
+++ Proj-RoadTrip.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proj_RoadTrip.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proj_RoadTrip.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
